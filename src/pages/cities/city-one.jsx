fb2b2a69c0c6812050da4f60183a4c5918fa9649
guess_ana <32547479+Gesana@users.noreply.github.com>
2021-05-16 19:18:58 +0200

    temp text

--- src/pages/cities/city-one.jsx
+++ src/pages/cities/city-one.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function CityOne() {
   return (
     <div className="city-one">
+      city one page
       <img src="../../images/Despina-3.png" alt="" />
     </div>
   );
