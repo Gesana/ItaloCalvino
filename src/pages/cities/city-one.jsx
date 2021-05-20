75d310dc2ac0a377efa66982fe456e29bc25514c
guess_ana <32547479+Gesana@users.noreply.github.com>
2021-05-20 20:13:57 +0200

    ship

--- src/pages/cities/city-one.jsx
+++ src/pages/cities/city-one.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import $ from "jquery";
 
 function CityOne() {
+  const pos = { x: 0, y: 0 };
+
+  const saveCursorPosition = function (x, y) {
+    pos.x = (x / window.innerWidth).toFixed(2);
+    pos.y = (y / window.innerHeight).toFixed(2);
+    document.documentElement.style.setProperty("--x", pos.x);
+    document.documentElement.style.setProperty("--y", pos.y);
+  };
+
+  document.addEventListener("mousemove", (e) => {
+    saveCursorPosition(e.clientX, e.clientY);
+  });
   return (
-    <div className="city-one">
-      city one page
-      <img src="../../images/Despina-3.png" alt="" />
+    <div className="city-one" id="ship">
+      <img src={"../../public/images/DespinaShip.png"} alt="moving image" />
     </div>
   );
 }
